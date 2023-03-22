34c1c07209cea5569d74c98ad54952aa53d97e85
Meggin <megginsimon@gmail.com>
2023-03-22 18:36:39 +0000

    2015, day 2 refactor

--- 2015_2.py
+++ 2015_2.py
@@ -1,16 +1,12 @@
 with open("2015_2_input.txt") as file:
     presents = file.readlines()
 
-paper_total = 0
-ribbon_total = 0
+paper_total = ribbon_total = 0
 
 for present in presents:
     l, w, h = sorted(map(int, present.split("x")))
-    lw, wh, hl = (l*w), (w*h), (h*l)
-    present_sqft = (2*lw + 2*wh + 2*hl) + min([lw, wh, hl])
-    paper_total += present_sqft
-    ribbon = (l*2 + w *2) + l*w*h
-    ribbon_total += ribbon
+    paper_total += (2*l*w + 2*w*h + 2*h*l) + min([l*w, w*h, h*l])
+    ribbon_total += (l*2 + w *2) + l*w*h
 
 print(f'Sqft of wrapping paper needed:{paper_total}')
 print(f'Ribbon needed:{ribbon_total}')
